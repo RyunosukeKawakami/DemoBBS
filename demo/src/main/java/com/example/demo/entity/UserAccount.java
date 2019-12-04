3a983297a739e09386b333c23bd19ee883b2d4a7
RyunosukeKawakami <k.r.doragon28@gmail.com>
2019-12-05 00:30:48 +0900

    passwordのスペルミスとLombokのgetter,setterを削除

--- demo/src/main/java/com/example/demo/entity/UserAccount.java
+++ demo/src/main/java/com/example/demo/entity/UserAccount.java
@@ -8,26 +8,25 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import lombok.Getter;
-import lombok.Setter;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="user_account")
-public class UserAccount implements Serializable{
+public class UserAccount{
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name="userid")
-    private String userID;
+    @Column(name = "userid")
+    private int userID;
     @Column(name="password")
-    private String passward;
+    private String password;
+    @NotBlank
     @Column(name="user_name")
     private String userName;
 
-    public void setUserID(String userID) {this.userID = userID;}
-    public String getUserID() {return userID;}
-    public void setPassword(String password) {this.passward = password;}
-    public String getPassword() {return passward;}
+    public void setUserID(int userID) {this.userID = userID;}
+    public int getUserID() {return userID;}
+    public void setPassword(String password) {this.password = password;}
+    public String getPassword() {return password;}
     public void setUserName(String userName) {this.userName = userName;}
     public String getUserName() {return userName;}
 }
