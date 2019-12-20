320eaf0a2ab2c6ec47a1991df929689084839e6f
RyunosukeKawakami <k.r.doragon28@gmail.com>
2019-12-20 11:58:29 +0900

    Postマッピングに対応するメソッドを追加

--- demo/src/main/java/com/example/demo/controller/LoginController.java
+++ demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,23 +1,40 @@
+package com.example.demo.controller;
+
 import com.example.demo.repository.UserAccountRepository;
 import com.example.demo.entity.UserAccount;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Controller
-@RequestMapping(value={"login/index.htaml","/login","login/"})
-public class LoginController {
+@RequestMapping(value={"login/index.html","/login","login/"})
+public class LoginController{
     @Autowired
     UserAccountRepository userAccountRepository;    
 
-    @GetMapping
+    @GetMapping    
     public ModelAndView ReturnView(@ModelAttribute("UserAccount") UserAccount account, ModelAndView model) {
         model.addObject("UserAccount", account);
         model.setViewName("login/index.html");
+        return model;
+    }
+
+    @PostMapping
+    public ModelAndView Login(@ModelAttribute("UserAccount") @Validated UserAccount account, BindingResult result,
+            ModelAndView model) {
+        if (result.hasErrors()) {
+            model.setViewName("login/index.html");
+        }
+        model.setViewName("forward:loginProcess");
+    
         return model;
     }
 }
