3fc6a2625ef3b8846abc23c6f526c3625d065a3a
RyunosukeKawakami <k.r.doragon28@gmail.com>
2019-12-25 17:06:21 +0900

    戻り値の型をModelAndViewに統一

--- demo/src/main/java/com/example/demo/controller/ReturnPageController.java
+++ demo/src/main/java/com/example/demo/controller/ReturnPageController.java
@@ -4,22 +4,26 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ReturnPageController {
     @RequestMapping(value = "/index.html")
-    public Resource ReturnHomeHTML() {
-        return new ClassPathResource("index.html");
+    public ModelAndView ReturnHomeHTML(ModelAndView model) {
+        model.setViewName("index.html");
+        return model;
     }
 
     @RequestMapping(value = "/topic")
-    public Resource ReturnTopicHTML() {
-        return new ClassPathResource("topic/index.html");
+    public ModelAndView ReturnTopicHTML(ModelAndView model) {
+        model.setViewName("topic/index.html");
+        return model;
     }
 
     @RequestMapping(value = "/contact")
-    public Resource ReturnContactHTML() {
-        return new ClassPathResource("contact/index.html");
+    public ModelAndView ieturnContactHTML(ModelAndView model) {
+        model.setViewName("contact/index.html");
+        return model;
     }
 }
     
