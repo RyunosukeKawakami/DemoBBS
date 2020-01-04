92792707ab94912d74d94c7e818afe96246059a3
RyunosukeKawakami <k.r.doragon28@gmail.com>
2020-01-04 12:45:43 +0900

    Titleを取得するときのNullPointerExceptionを修正

--- demo/src/main/java/com/example/demo/service/ThreadSave.java
+++ demo/src/main/java/com/example/demo/service/ThreadSave.java
@@ -1,7 +1,5 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.UserAccount;
-
 import java.security.Principal;
 import java.util.Date;
 
@@ -9,21 +7,28 @@ import com.example.demo.entity.Thread;
 import com.example.demo.repository.ThreadRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 /**
  * ThreadSave
  * ThreadControllerで受け取ったタイトル名から、ログイン名、作成日を確認して
  * DBに保存するクラス
  */
+@Service
 public class ThreadSave {
     @Autowired
     ThreadRepository repository;    
+    UserDetails account;
     Thread thread;
-    UserAccount account;
 
-    public void SaveThread(Principal principal, String title) {
-        thread.setTitle(title);
-        thread.setAuthor(principal.getName());
+    public void SaveThread(Principal principal, Thread thread) {
+        Authentication auth = (Authentication)principal;
+        account = (UserDetails)auth.getPrincipal();
+
+        thread.setTitle(thread.getTitle());
+        thread.setAuthor(account.getUsername());
         thread.setCreateDate(new Date());
 
         repository.save(thread);
