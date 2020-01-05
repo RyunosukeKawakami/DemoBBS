38d0892b8c2aa096e78d422001ceaed76d284fb6
RyunosukeKawakami <k.r.doragon28@gmail.com>
2020-01-05 19:29:03 +0900

    スレッドを削除できるようにした

--- demo/src/main/java/com/example/demo/controller/TopicController.java
+++ demo/src/main/java/com/example/demo/controller/TopicController.java
@@ -12,7 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -37,9 +39,14 @@ public class TopicController {
         return model;
     }
     
-    @DeleteMapping
-    public ModelAndView DeleteThread(ModelAndView model) {
-                
+    @PostMapping
+    public ModelAndView DeleteThread(@RequestParam("id") int id, ModelAndView model) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        }
+
+        model.addObject("ThreadList", threadList);
+        model.setViewName("redirect:/topic");
         return model;
     }
 }
