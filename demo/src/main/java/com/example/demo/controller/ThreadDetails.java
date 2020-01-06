646f544d5e7f7923bedfe7dcda88222e683c2184
RyunosukeKawakami <k.r.doragon28@gmail.com>
2020-01-06 18:11:53 +0900

    タイトルから、各スレッドに飛べるように

--- demo/src/main/java/com/example/demo/controller/ThreadDetails.java
+++ demo/src/main/java/com/example/demo/controller/ThreadDetails.java
@@ -1,17 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * ThreadDetails
  */
-@RequestMapping(value="/topic/thread/{id}")
+@RequestMapping(path = "/topic/thread/{id}")
+@Controller
 public class ThreadDetails {
     @GetMapping
-    public ModelAndView ReturnThreadDetails(ModelAndView model) {
-        model.setViewName("/topic/thread.html");
+    public ModelAndView ReturnThreadDetails(@PathVariable int id, ModelAndView model) {
+        model.setViewName("topic/thread.html");
         return model;
     }
 
