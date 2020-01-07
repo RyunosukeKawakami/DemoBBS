48578dac9a5c26d1dcf16db3f31b8687c65d4bdc
RyunosukeKawakami <k.r.doragon28@gmail.com>
2020-01-07 19:27:05 +0900

    threadControllerを作成

--- demo/src/main/java/com/example/demo/controller/ThreadController.java
+++ demo/src/main/java/com/example/demo/controller/ThreadController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import com.example.demo.entity.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +17,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(path = "/topic/thread/{id}")
 @Controller
 public class ThreadController {
+
+    @Autowired
+    List<Response> response;
+
     @GetMapping
     public ModelAndView ReturnThreadDetails(@PathVariable int id, ModelAndView model) {
+        model.addObject("ResponseList", response);
         model.setViewName("topic/thread.html");
         return model;
     }
