7f4a29088065fe02634632ab133086a6de3d34ad
RyunosukeKawakami <k.r.doragon28@gmail.com>
2020-01-11 16:52:21 +0900

    スレッドを削除すると、レスポンスも削除するように修正

--- demo/src/main/java/com/example/demo/repository/ResponseRepository.java
+++ demo/src/main/java/com/example/demo/repository/ResponseRepository.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import com.example.demo.entity.Response;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,5 +17,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ResponseRepository extends JpaRepository<Response, Integer> {
     public List<Response> findAllByThreadId(int thread_id);
     public Response findById(int id);
+
     public Boolean existsByThreadId(int thread_id);
+    @Transactional
+    public void deleteByThreadId(int thread_id);
 }
